320bd7e7dbc8d9fa2b62d02b7f4b4aa46f12a774
Soulravager <alwinkuttikkattu@gmail.com>
2025-08-22 11:23:54 +0530

    -fix softdelete

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\SoftDeletes; 
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasUuids;
+    use HasApiTokens, HasFactory, Notifiable, HasUuids,SoftDeletes;
 
     protected $keyType = 'string';
     public $incrementing = false;
--- database/migrations/2025_08_12_153527_create_products_table.php
+++ database/migrations/2025_08_12_153527_create_products_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
         $table->integer('stock')->default(0);
         $table->string('image')->nullable(); 
         $table->timestamps();
+        $table->softDeletes();
     });
 }
 
