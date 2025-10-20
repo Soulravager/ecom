aaba13090a7f5800b8012391d7a7a0a7f717d31b
Soulravager <alwinkuttikkattu@gmail.com>
2025-10-20 19:50:29 +0530

    -fix

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -9,36 +9,46 @@ use App\Http\Requests\UpdateProductRequest;
 
 class ProductController extends Controller
 {
+
     public function index()
     {
-        return response()->json(Product::all());
+        $products = Product::all()->map(function ($product) {
+            $product->image = $product->image
+                ? url('storage/' . $product->image) 
+                : null;
+            return $product;
+        });
+
+        return response()->json($products);
     }
-
-
-
-
 
     public function store(ProductRequest $request)
     {
         $productData = $request->validated();
+
+        if ($request->hasFile('image')) {
+            $path = $request->file('image')->store('products', 'public');
+            $productData['image'] = $path;
+        }
+
         $product = Product::create($productData);
+
+
+        $product->image = $product->image
+            ? url('storage/' . $product->image)
+            : null;
 
         return response()->json($product, 201);
     }
-
-
-
-
-
-
-
     public function show($id)
     {
         $product = Product::findOrFail($id);
+        $product->image = $product->image
+            ? url('storage/' . $product->image)
+            : null;
+
         return response()->json($product);
     }
-
-
 
 
     public function update(UpdateProductRequest $request, $id)
@@ -46,17 +56,27 @@ class ProductController extends Controller
         $product = Product::findOrFail($id);
         $updateData = $request->validated();
 
+        if ($request->hasFile('image')) {
+            $path = $request->file('image')->store('products', 'public');
+            $updateData['image'] = $path;
+        }
+
         $product->update($updateData);
+
+   
+        $product->image = $product->image
+            ? url('storage/' . $product->image)
+            : null;
 
         return response()->json($product);
     }
 
-
-
-
+    
     public function destroy($id)
     {
-        Product::findOrFail($id)->delete();
-        return response()->json(['message' => 'Product deleted']);
+        $product = Product::findOrFail($id);
+        $product->delete();
+
+        return response()->json(['message' => 'Product deleted successfully']);
     }
 }
